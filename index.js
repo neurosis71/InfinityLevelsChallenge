315844d897987316055d869982a8ebda411a24e3
neurosis71 <neurosis71@hotmail.fr>
2015-08-09 21:36:47 +0200

    CLI program to process the log file and display stats

--- index.js
+++ index.js
@@ -1,43 +1,29 @@
-//required modules
-var fs = require('fs');
+/********** required modules *************/
+var processor = require('logProcessor');
+var EventEmitter = require('events').EventEmitter;
+var stats = require('stats');
+
+/********** initialization ***************/
+
+// Object to pass through the processing and reports function to store data
+processorData = {
+	"cpm" : {"searchFor" : "count_pending_messages", "count" : 0, "timeResponses" : [], "dynos" : []},// for GET /api/users/{user_id}/count_pending_messages
+	"gm" : {"searchFor" : "get_messages", "count" : 0, "timeResponses" : [], "dynos" : []},//for GET /api/users/{user_id}/get_messages
+	"gfp" : {"searchFor" : "get_friends_progress", "count" : 0, "timeResponses" : [], "dynos" : []},//for GET /api/users/{user_id}/get_friends_progress
+	"gfs" : {"searchFor" : "get_friends_score", "count" : 0, "timeResponses" : [], "dynos" : []},//for GET /api/users/{user_id}/get_friends_score
+	"pusers" : {"searchFor" : "/api/users", "searchFor2" : "method=POST", "count" : 0, "timeResponses" : [], "dynos" : []},//for POST /api/users/{user_id}
+	"gusers" : {"searchFor" : "/api/users", "searchFor2" : "method=GET", "count" : 0, "timeResponses" : [], "dynos" : []} //for GET /api/users/{user_id}
+	};
+
+//var for event management in logProcessor
+var processorEvent = new EventEmitter();
 
 
-//initialization
-var stream = fs.createReadStream('resources/sample.log', {flags: 'r', encoding: 'utf-8'});
-var buf = '';
+/*********** processing ***************/
 
+processor.processFile(processorEvent, processorData);
 
-//processing
-stream.on('data', function(d) {
-    buf += d.toString(); // when data is read, stash it in a string buffer
-    pump(); // then process the buffer
+processorEvent.on('DoneProcess', function(message){
+	stats.reportStats(processorData);	
 });
 
-
-
-/*********** Helper functions ***************/
-
-//verifies we don't have an empty line in  the buffer, if not process it
-function pump() {
-    var pos;
-
-    while ((pos = buf.indexOf('\n')) >= 0) { // keep going while there's a newline somewhere in the buffer
-        if (pos == 0) { // if there's more than one newline in a row, the buffer will now start with a newline
-            buf = buf.slice(1); // discard it
-            continue; // so that the next iteration will start with data
-        }
-        process(buf.slice(0,pos)); // hand off the line
-        buf = buf.slice(pos+1); // and slice the processed data off the buffer
-    }
-}
-
-
-//process the line in the buffer to create the stats
-function process(line) { // here's where we do something with a line
-
-    if (line[line.length-1] == '\r') line=line.substr(0,line.length-1); // discard CR (0x0D)
-
-    if (line.length > 0) { // ignore empty lines
-        console.log(line); 
-    }
-}
